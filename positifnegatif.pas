294d9a12037599d3e03a906635713ef2e63b9b72
rianbekooo <hiiyanbekoo@gmail.com>
2024-10-02 18:01:38 +0700

    Update positifnegatif.pas

--- positifnegatif.pas
+++ positifnegatif.pas
@@ -4,14 +4,15 @@ var
     n: integer;
 begin
     clrscr;
-    write('Masukkan bilangan bulat: '); readln(n);
+    write('Masukkan sebuah angka: ');
+    readln(n);
     if n>0 then
-    begin
-        writeln(n,' merupakan bilangan positif');
-    end
+        begin
+            writeln(n,' yaitu bilangan positif');
+        end
     else if n<0 then
-    begin
-        writeln(n,' merupakan bilangan negatif');
-    end;
+        begin
+            writeln(n,' yaitu bilangan negatif');
+        end;
     readln;
 end.
