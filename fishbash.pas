c0269849db36270b9931807d22d6d0b36a6e3527
rianbekooo <hiiyanbekoo@gmail.com>
2024-10-02 17:56:43 +0700

    Update fishbash.pas

--- fishbash.pas
+++ fishbash.pas
@@ -6,20 +6,20 @@ begin
     clrscr;
         Write('Masukkan sebuah angka: '); readln(angka);
     if (angka mod 3 = 0) and (angka mod 5 = 0) then
-    begin
-        Writeln('Fish Bash');
-    end
+      begin
+          Writeln('Fish Bash');
+      end
     else if (angka mod 3 = 0) then
-    begin
-        Writeln('Fish');
-    end
+      begin
+          Writeln('Fish');
+      end
     else if (angka mod 5 = 0) then
-    begin
-        writeln('Bash');
-    end
+      begin
+          writeln('Bash');
+      end
     else
-    begin
-        writeln(angka);
-    end;
-    readln;
+      begin
+          writeln(angka);
+      end;
+      readln;
 end.
