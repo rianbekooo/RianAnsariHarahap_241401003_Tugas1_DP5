147ec6b83f69bf77600528fbd39859275f5c8155
rianbekooo <hiiyanbekoo@gmail.com>
2024-10-02 18:03:15 +0700

    Update prima.pas

--- prima.pas
+++ prima.pas
@@ -1,24 +1,25 @@
-program bilprima;  
+program prima;  
 uses crt;
 var  
   angka, i, hasil: integer;    
 begin  
     clrscr;
-    writeln('Masukkan sebuah bilangan bulat:'); readln(angka);  
+    writeln('Masukkan angka: '); 
+    readln(angka);  
     if angka < 2 then  
-    begin  
-        writeln(angka, ' bukan bilangan prima.');
-    exit;  
-    end;
+      begin  
+        writeln(angka, ' tidak bilangan prima.');
+      exit;  
+      end;
     hasil := 0;  
     for i := 1 to angka do  
-    begin  
-    if (angka mod i = 0) then  
+      begin  
+      if (angka mod i = 0) then  
         hasil := hasil + 1;  
-    end;  
+      end;  
     if hasil = 2 then  
-        writeln(angka, ' adalah bilangan prima.')  
+        writeln(angka, ' yaitu bilangan prima.')  
     else  
-        writeln(angka, ' bukan bilangan prima.');  
+        writeln(angka, ' yaitu bukan bilangan prima.');  
     readln;
 end.
